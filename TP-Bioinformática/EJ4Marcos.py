c2421fe8a81d13ef2c93e9b0cac4be2c31f2b10b
LogOut <81473797+mt1665@users.noreply.github.com>
2024-10-19 14:47:01 -0300

    Update EJ4Marcos.py
    esta es la version final, se necesita tener los archivos de prosite "prosite.dat" y "prosite.doc" guardados en la misma carpeta llamada "PROSITE" para poder correr el prosextract, que es el paso previo al patmatmotifs. reemplazar el prosite_path por el directorio de la carpeta PROSITE. Para este programa se necesita lo de arriba+un archivo .gb de los nucleotidos del gen. Te devuelve dos archivos, el primero los orf posibles encontrados por el analisis de EMBOSS. El segundo es un archivo con todos los orfs que tuvieron al menos un motivo. Esta es la primera version, tengo que probar si funciona en linux

--- TP-Bioinformática/EJ4Marcos.py
+++ TP-Bioinformática/EJ4Marcos.py
@@ -1,24 +1,17 @@
 # -*- coding: utf-8 -*-
+import os
 import sys
+import shutil
 import subprocess
 from Bio import SeqIO
-import os
 
 GenBankPath = "opsina.gb"
 GenBank = SeqIO.read(GenBankPath, "genbank")
 nucleotide_sequence = str(GenBank.seq)
-print(nucleotide_sequence[:10])
 
 fasta_path = 'secuencia.fasta'
 with open(fasta_path, 'w') as fasta_file:
     fasta_file.write(f">Opsina_sequence\n{nucleotide_sequence}\n")
-
-prositeData_path = 'prosite.dat'
-with open(prositeData_path, 'r') as file:
-    prosite_db = file.read()
-
-orf_output = "orf_proteins.fasta"
-motifs_output = "motifs_results.txt"
 
 def run_command(command):
     """Ejecuta un comando en la terminal y retorna la salida"""
@@ -29,6 +22,7 @@ def run_command(command):
     return result.stdout
 
 """##ORF"""
+orf_output = "orf_proteins.fasta"
 
 if os.path.exists(orf_output):
     os.remove(orf_output)
@@ -49,50 +43,111 @@ else:
     orf_count = sum(1 for _ in SeqIO.parse(orf_output, "fasta"))
     print(f"Cantidad de ORFs encontrados: {orf_count}")
 
-orf_count = sum(1 for _ in SeqIO.parse(orf_output, "fasta"))
-print(f"Cantidad de ORFs encontrados: {orf_count}")
-
 # Llamar a EMBOSS 'geecee' para calcular el contenido de GC
 os.system('geecee -sequence secuencia.fasta -outfile resultado_gc.txt')
 
-# Mostrar el contenido del archivo de resultados
-with open('resultado_gc.txt', 'r') as result_file:
-    print(result_file.read())
-
-#procesamiento manual
-Citocina = nucleotide_sequence.count("C")
-Adenina = nucleotide_sequence.count("A")
-Timina = nucleotide_sequence.count("T")
-Guanina = nucleotide_sequence.count("G")
-print("Cantidad de Citocina:", Citocina)
-print("Cantidad de Adenina:", Adenina)
-print("Cantidad de Timina:", Timina)
-print("Cantidad de Guanina:", Guanina)
-Total = Citocina + Adenina + Timina + Guanina
-print("Total de nucleotidos:", Total)
-print("Contenido de G o C:", (Citocina + Guanina) / Total * 100, "%")
-
 # Contar la cantidad de veces que aparece la secuencia "GCA" en la secuencia de nucleótidos
 ATG_count = nucleotide_sequence.count("ATG")
-
-print(f'La secuencia "ATG" aparece {ATG_count} veces en la cadena de nucleótidos.')
-
-# Contar la cantidad de veces que aparece la secuencia "GCA" en la secuencia de nucleótidos
 stop_count = nucleotide_sequence.count("TGA")+nucleotide_sequence.count("TAA")+nucleotide_sequence.count("TAG")
 
+print(f'La secuencia "start" aparece {ATG_count} veces en la cadena de nucleótidos.')
 print(f'La secuencia "stop" aparece {stop_count} veces en la cadena de nucleótidos.')
+
+# Contar la cantidad de veces que aparece la secuencia "GCA" en la secuencia de nucleótidos
 
 """##Motif"""
 
-# Ejecuta patmatmotifs para analizar los motivos en las secuencias de proteínas
-print("Analizando motivos en las secuencias de proteínas obtenidas...")
-patmatmotifs_command = f"patmatmotifs -sequence {orf_output} -db {prosite_db} -outfile {motifs_output}"
-patmatmotifs_result = run_command(patmatmotifs_command)
+motifs_output = "motifs_individual_results.txt"
+motifs_final_output = "motifs_final_results.txt"
+prosite_path = "/PROSITE"
+ruta_copia = 'orf_copia.fasta'
 
-# Verifica si patmatmotifs se ejecutó correctamente
-if patmatmotifs_result is None:
-    print("Error al ejecutar patmatmotifs.")
-else:
-    print(f"Análisis de motivos completado. Resultados guardados en {motifs_output}")
+shutil.copy(orf_output, ruta_copia)
 
-print(f"Análisis de motivos completado. Resultados guardados en {motifs_output}")
+def individual_orf(archivo_orfs_path):
+  if os.path.exists("orf_individual.fasta"):
+    # Si el archivo existe, eliminarlo
+    os.remove("orf_individual.fasta")
+
+  ruta_salida = 'orf_individual.fasta'
+  # Leer el archivo de entrada
+  with open(archivo_orfs_path, 'r') as archivo_entrada:
+      lineas = archivo_entrada.readlines()
+
+  # Lista para almacenar las líneas del primer ORF
+  lineas_a_borrar = []
+  lineas_restantes = []
+
+  # Variables de control
+  esta_guardando_orf = False
+
+  # Recorrer las líneas y clasificar entre las que se guardarán y las que se conservarán
+  for linea in lineas:
+      # Si la línea comienza con ">", indica el inicio de un ORF
+      if linea.startswith('>'):
+          # Si ya estábamos guardando el primer ORF, dejamos de guardar y pasamos al siguiente
+          if esta_guardando_orf:
+              esta_guardando_orf = False
+          # Si no estábamos guardando, es el primer ORF que encontramos, así que empezamos a guardar
+          elif not lineas_a_borrar:
+              esta_guardando_orf = True
+
+      # Guardar las líneas del primer ORF o del resto
+      if esta_guardando_orf:
+          lineas_a_borrar.append(linea)
+      else:
+          lineas_restantes.append(linea)
+
+  # Guardar las líneas del primer ORF en el archivo de salida
+  with open(ruta_salida, 'w') as archivo_salida:
+      archivo_salida.writelines(lineas_a_borrar)
+
+  # Sobrescribir el archivo original con las líneas restantes
+  with open(archivo_orfs_path, 'w') as archivo_entrada:
+      archivo_entrada.writelines(lineas_restantes)
+
+prosextract_command = f"prosextract -prositedir {prosite_path}"
+prosextract_result = run_command(prosextract_command)
+
+i=1
+while i<=orf_count:
+  print("/n", i, "/n" )
+  individual_orf('orf_copia.fasta')
+
+
+  # Ejecuta patmatmotifs para analizar los motivos en las secuencias de proteínas
+  print(f"Analizando motivos de la secuencia {i}")
+  orf_output = "orf_individual.fasta"
+  patmatmotifs_command = f"patmatmotifs -sequence {orf_output} -outfile {motifs_output}"
+  patmatmotifs_result = run_command(patmatmotifs_command)
+
+  # Verifica si patmatmotifs se ejecutó correctamente
+  if patmatmotifs_result is None:
+      print("Error al ejecutar patmatmotifs.")
+
+  with open(motifs_output, 'r') as archivo:
+      for linea in archivo:
+          if 'HitCount:' in linea:
+              # Extraer el valor del HitCount
+              hit_count = linea.split(':')[-1].strip()
+              print(f'HitCount: {hit_count}')
+  if hit_count == "0":
+    print("No se encontraron coincidencias.")
+  else:
+
+    # Leer el contenido del archivo fuente
+    with open(motifs_output, 'r') as archivo_fuente:
+        contenido = archivo_fuente.read()
+
+    # Abrir el archivo destino en modo 'append' y agregar el contenido
+    with open(motifs_final_output, 'a') as archivo_destino:
+        archivo_destino.write(contenido)
+
+    print(f'El contenido de {motifs_output} se ha agregado al final de {motifs_final_output}.')
+
+  i=i+1
+
+os.remove("motifs_individual_results.txt")
+os.remove("orf_individual.fasta")
+os.remove("orf_copia.fasta")
+os.remove("secuencia.fasta")
